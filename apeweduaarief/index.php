819cb3e328b259f4bf98b22d56c2254016018ad7
arief98 <ariefrahmanbatara@gmail.com>
2018-03-20 22:42:02 +0700

    latihan 1 halaman perdana

---
+++ apeweduaarief/index.php
@@ -0,0 +1,17 @@
+<?php
+//ini adalah komentar saja
+//bagaimana bila 
+//engkau wahai komentar
+//berpanjang lebar dalam berkata-kata
+echo "Arief Rahman<br>";
+echo "Nim H1101161014<br>";
+
+echo "Selamat pagi dunia !";
+echo "Pagi !<br>";
+
+$aaa= 54321;
+
+echo "nim saya adalah ".$aaa;
+
+
+?>
